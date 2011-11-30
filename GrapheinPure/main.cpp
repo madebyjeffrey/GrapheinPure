3f9a0c01b5b0ae8db1a4d78fbd1b0355b44cf08f
Jeffrey Drake <iaefai@me.com>
2011-11-30 14:41:25 -0500

    Initial Commit

---
+++ GrapheinPure/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GrapheinPure
+//
+//  Created by Jeffrey Drake on 11-11-30.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
